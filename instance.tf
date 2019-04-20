a136cb82bb60d415ac40d44717bc581f08c044bb
addiani <addiani744@gmail.com>
2019-04-20 15:20:28 -0500

    change the code to reusable code

--- instance.tf
+++ instance.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "web" {
-  ami = "ami-07683a44e80cd32c5"
-  instance_type = "t2.micro"
-  count = 5
-  key_name = "${aws_key_pair.developer.key_name}"
+  ami = "${var.ami}"
+  instance_type = "${var.instance_type}"
+  count = "${var.count}"
+  key_name = "${var.key_name}"
   tags {
-    Name = "terraform-january-addiani"
-    Env  = "dev"
-    Dept = "IT"
-    Created_by = "addiani"
+    Name = "terraform-january-${var.Created_by}-${count.index +1}"
+    Env  = "${var.Env}"
+    Dept = "${var.Dept}"
+    Created_by = "${var.Created_by}"
   }
 }
