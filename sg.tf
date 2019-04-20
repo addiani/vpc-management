31c2e07e2408c02da5d447b5c14a6d6cefb6d361
addiani <addiani744@gmail.com>
2019-04-20 17:01:52 -0500

    added sg list

---
+++ sg.tf
@@ -0,0 +1,15 @@
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow ssh inbound traffic"
+  vpc_id      = "${aws_vpc.dev.id}"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks  = ["0.0.0.0/0"]
+  egress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "-1"
+    cidr_blocks  = ["0.0.0.0/0"]
