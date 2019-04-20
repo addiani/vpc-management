8002ab7c887994121cf7d9653c7086133ff34264
addiani <addiani744@gmail.com>
2019-04-20 17:10:20 -0500

    added

--- sg.tf
+++ sg.tf
@@ -9,7 +9,7 @@ resource "aws_security_group" "allow_ssh" {
     protocol    = "tcp"
     cidr_blocks  = ["0.0.0.0/0"]
   egress {
-    from_port   = 22
-    to_port     = 22
+    from_port   = 0
+    to_port     = 0
     protocol    = "-1"
     cidr_blocks  = ["0.0.0.0/0"]
