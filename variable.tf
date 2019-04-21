fbabd4b941239e7977f19ff2534d62dd3d05d013
addiani <addiani744@gmail.com>
2019-04-21 10:51:40 -0500

    added variables

--- variable.tf
+++ variable.tf
@@ -5,3 +5,7 @@ variable "key_name" {}
 variable "Created_by" {}
 variable "Env" {}
 variable "Dept" {}
+variable "min_size" {}
+variable "max_size" {}
+variable "Appname" {}
+variable "desired_capacity"{}
