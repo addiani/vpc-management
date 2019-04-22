de0a29f8c1a31881cf6fec91676c6013e19ea201
addiani <addiani744@gmail.com>
2019-04-22 13:21:14 -0500

    added

--- output.tf
+++ output.tf
@@ -2,5 +2,5 @@ output "bucketname" {
   value = "S3 bucket  ${aws_s3_bucket.newbucket.bucket}   created"
 }
 output "public_ips" {
-     value = "wordpress instances are ${aws_instance.web.*.host_dns}"
+     value = " ${aws_instance.web.*.public_ip}"
 }
